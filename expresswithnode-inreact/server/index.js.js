6123db018f5d60365c5006a2dd682b54592041bf
Aman-dhiman2 <73783000+Aman-dhiman2@users.noreply.github.com>
2022-10-31 10:27:28 +0530

    Add files via upload

---
+++ expresswithnode-inreact/server/index.js.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  res.json({ message: "This is my home page" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
